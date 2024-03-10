e9e47418478a8926e01e36c955abb2c0f25864fe
Phương <20521779@gm.uit.edu.vn>
2024-03-10 19:36:09 +0700

    commit all pages

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-const cardService = document.querySelectorAll(".card-service");
+const navLinks = document.querySelector(".nav-links");
+function ToggleMenu(e) {
+  e.textContent = e.textContent === "menu" ? "close" : "menu";
+  navLinks.classList.toggle("top-full");
+}
