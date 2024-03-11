181f44d6d39e674b13101e75238b0835700a204a
Phương <20521779@gm.uit.edu.vn>
2024-03-11 15:05:07 +0700

    header and effect

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-const navLinks = document.querySelector(".nav-links");
-function ToggleMenu(e) {
-  e.textContent = e.textContent === "menu" ? "close" : "menu";
-  navLinks.classList.toggle("top-full");
+const currentPage = window.location.pathname;
+const links = document.querySelectorAll(".header-line li");
+links.forEach((li) => {
+  const link = li.querySelector("a");
+  const href = link.getAttribute("href");
+  if (currentPage === href) {
+    li.classList.add("selected-link");
+  }
+});
+console.log(currentPage);
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  if (sidebar) {
+    sidebar.classList.add("active");
+  }
 }
+function hideSidebar() {
+  const close = document.querySelector(".close-icon");
+  const sidebar = document.querySelector(".sidebar");
+  if (sidebar) {
+    close.addEventListener("click", function () {
+      sidebar.classList.remove("active");
+    });
+  }
+}
+// function handleNav(e) {
+//   // Remove selected-link class from all links
+//   links = document.querySelectorAll(".header-line li");
+//   links.forEach((link) => {
+//     link.classList.remove("selected-link");
+//   });
+//   e.classList.add("selected-link");
+// }
