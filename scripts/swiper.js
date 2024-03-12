531a6ab31a964dc0802bc9add036c02f40080637
Phương <20521779@gm.uit.edu.vn>
2024-03-12 13:39:08 +0700

    commit

---
+++ scripts/swiper.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //   autoplay: {
+  //     delay: 2500,
+  //     disableOnInteraction: false,
+  //   },
+});
+var swiper1 = new Swiper(".mySwiper1", {
+  slidesPerView: 3,
+  spaceBetween: 15,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 35,
+    },
+    1025: {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    },
+  },
+});
+swiper1.loopDestroy();
+swiper1.loopCreate();
